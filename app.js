f015f44041fe86b364516e3e5586e6a96c56e364
Sergey Dmitriev <serge.dmitriev@gmail.com>
2015-12-21 17:20:32 +0700

    fix

--- app.js
+++ app.js
@@ -3,25 +3,28 @@ var AsteriskManager = require('asterisk-manager');
 var Q = require('q');
 
 var ami = new AsteriskManager('5038','localhost','admin','admin', true);  
-//var ami = new AsteriskManager('5038','p02.mobilon.ru','amiadmin','kug09han', true);  
+ 
 // In case of any connectiviy problems we got you coverd. 
 ami.keepConnected();
 
 var sockets = [];
 
 ami.on('dial', function (evt) {
-	console.log('dial', evt.subevent, evt.uniqueid);
+	console.log('dial', evt.subevent, evt.uniqueid, evt.sequencenumber);
 	sockets.map(function (socket) {
-		socket.emit(evt);
+		socket.emit('data', evt);
 	});
 });
 
 
-var app = require('express')();
+var express = require('express');
+var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 var config = require('./config');
+
+app.use('/bower_components', express["static"](__dirname + "/bower_components"));
 
 app.get('/', function(req, res){
   res.sendfile('index.html');
